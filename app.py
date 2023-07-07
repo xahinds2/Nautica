3f3e334b9196838f48c792d3cc48424d05cc0752
Sahin <xahinds2@gmail.com>
2023-07-07 09:43:01 +0530

    changes for app.py

--- app.py
+++ app.py
@@ -1,53 +1,107 @@
-from utils.userdata import verifyUser, addUser
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 from utils.search import flipkartSearch, amazonSearch
 from utils.proccess import populate_data
-from flask import Flask, render_template, redirect, url_for
-from flask import request
-import pandas as pd
 import time
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
+def validate_username(username):
+    existing_user_username = User.query.filter_by(
+        username=username.data).first()
+    if existing_user_username:
+        raise ValidationError(
+            'That username already exists. Please choose a different one.')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)],
+                             render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
 
 
 @app.route('/')
 def home():
-    return redirect(url_for('login'))
+    return render_template('home.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-
-        username = request.form['username']
-        password = request.form['password']
-
-        if verifyUser(username, password):
-            return redirect(url_for('search'))
-
-        return render_template('login.html')
-
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
 
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
 
-        if addUser(username, password, email):
-            return redirect(url_for('search'))
-
-        return redirect(url_for('signup'))
-
-    return render_template('signup.html')
+    return render_template('register.html', form=form)
 
 
-@app.route("/search", methods=["POST", "GET"])
-def search():
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
     if request.method == "POST":
 
         q = request.form["product_name"]
@@ -64,24 +118,9 @@ def search():
 
         values = list(data.values)
         data.to_csv('logs/datalog.csv')
-        return render_template('index.html', stocklist=values)
+        return render_template('dashboard.html', stocklist=values)
 
-    return render_template("index.html")
-
-
-@app.route('/stocks')
-def Stocks():
-    filename = 'logs/datalog.csv'
-    data = pd.read_csv(filename)
-    data.pop('Unnamed: 0')
-    values = list(data.values)
-    return render_template('stocks.html', stocklist=values)
-
-
-@app.route("/<usr>")
-def user(usr):
-    x = "127.0. 0.1:5000/" + usr
-    return f"<h1>Please check the URL once : {x}</h1>"
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
