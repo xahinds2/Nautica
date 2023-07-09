afdcaceb2557d1e0857bf2ec3709ee8b75857407
Sahin <xahinds2@gmail.com>
2023-07-09 12:22:37 +0530

    user management added

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from utils.proccess import search_product
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -12,7 +11,6 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -23,10 +21,11 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 
 @login_manager.user_loader
@@ -61,20 +60,21 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
+        name = request.form['name']
 
         previous_user = User.query.filter_by(username=username).first()
+
         if previous_user:
             return render_template('signup.html',
                                    error='Username already exist!')
 
         hashed_password = bcrypt.generate_password_hash(password)
-        new_user = User(username=username, password=hashed_password)
+        new_user = User(username=username, password=hashed_password, name=name, email=email)
         db.session.add(new_user)
         db.session.commit()
-
         return redirect(url_for('login'))
 
     return render_template('signup.html')
@@ -94,13 +94,30 @@ def dashboard():
         q = request.form["product_name"]
         values = search_product(q)
         return render_template('dashboard.html', stocklist=values)
-
     return render_template('dashboard.html')
 
 
-@app.route('/users')
+@app.route('/delete_user/<username>')
+def delete_user(username):
+    user = User.query.filter_by(username=username).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('users'))
+
+
+@app.route('/users/')
+@login_required
 def users():
-    return render_template('users.html')
+    user_list = User.query.all()
+    for i in range(len(user_list)):
+        user = {
+            'name': user_list[i].name,
+            'email': user_list[i].email,
+            'username': user_list[i].username
+        }
+        user_list[i] = user
+    return render_template('users.html', user_list=user_list)
 
 
 if __name__ == "__main__":
