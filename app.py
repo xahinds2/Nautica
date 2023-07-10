b065aeec3ad3e6f3e2ba301e2179331581fe1089
Sahin <xahinds2@gmail.com>
2023-07-10 16:16:45 +0530

    sql to mongo db

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
+from bson import ObjectId
 from flask_bcrypt import Bcrypt
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask import Flask, render_template, url_for, redirect, request
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 from utils.proccess import search_product
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
-db = SQLAlchemy(app)
+client = MongoClient('mongodb+srv://xahinds2:Sahindas1%40@expensemanager.gcbsdlg.mongodb.net/')
+db = client['ExpenseApp']
+collection = db['users']
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
@@ -17,20 +18,19 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+        self.password = user_data['password']
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user_data = collection.find_one({'_id': ObjectId(user_id)})
+    if user_data:
+        return User(user_data)
+    return None
 
 
 @app.route('/')
@@ -45,10 +45,10 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = User.query.filter_by(username=username).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, password):
-                login_user(user)
+        user_data = collection.find_one({'username': username})
+        if user_data:
+            if bcrypt.check_password_hash(user_data['password'], password):
+                login_user(User(user_data))
                 return redirect(url_for('dashboard'))
 
         return render_template('login.html',
@@ -65,16 +65,20 @@ def signup():
         email = request.form['email']
         name = request.form['name']
 
-        previous_user = User.query.filter_by(username=username).first()
+        user_data = collection.find_one({'username': username})
 
-        if previous_user:
+        if user_data:
             return render_template('signup.html',
                                    error='Username already exist!')
 
         hashed_password = bcrypt.generate_password_hash(password)
-        new_user = User(username=username, password=hashed_password, name=name, email=email)
-        db.session.add(new_user)
-        db.session.commit()
+        user_data = {
+            'username': username,
+            'password': hashed_password,
+            'name': name,
+            'email': email
+        }
+        collection.insert_one(user_data)
         return redirect(url_for('login'))
 
     return render_template('signup.html')
@@ -99,25 +103,14 @@ def dashboard():
 
 @app.route('/delete_user/<username>')
 def delete_user(username):
-    user = User.query.filter_by(username=username).first()
-    if user:
-        db.session.delete(user)
-        db.session.commit()
+    collection.delete_one({'username': username})
     return redirect(url_for('users'))
 
 
 @app.route('/users/')
 @login_required
 def users():
-    user_list = User.query.all()
-    for i in range(len(user_list)):
-        user = {
-            'name': user_list[i].name,
-            'email': user_list[i].email,
-            'username': user_list[i].username,
-            'role': 'Admin' if current_user.username == user_list[i].username else 'User'
-        }
-        user_list[i] = user
+    user_list = collection.find()
     return render_template('users.html', user_list=user_list)
 
 
